ffedc347cbc0625e8764c2c5137e0f19f6674d24
Florent Buisson <florent.buisson.1987@gmail.com>
2017-09-13 14:54:23 +0000

    Scripts for TX2 compatible face or hands demos.

---
+++ face.sh
@@ -0,0 +1,1 @@
+./build/examples/openpose/openpose.bin -camera_resolution 640x480 -net_resolution 128x96 --face -face_net_resolution 256x256
---
+++ hands.sh
@@ -0,0 +1,1 @@
+./build/examples/openpose/openpose.bin -camera_resolution 640x480 -net_resolution 128x96 --hand -hand_net_resolution 256x256
