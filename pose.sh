58a789de41eef5e9b4e88866541ff35dd89ee148
Florent Buisson <florent.buisson.1987@gmail.com>
2018-02-03 13:02:10 +0000

    Gradual colors

--- pose.sh
+++ pose.sh
@@ -1,1 +1,1 @@
-./build/examples/openpose/openpose.bin -camera_resolution 640x480 -net_resolution 128x96
+./build/examples/openpose/openpose.bin -camera_resolution 640x480 -net_resolution 128x96 -alpha_pose 1
